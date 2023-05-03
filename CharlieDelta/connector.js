31e9c42451860b6bc4350b6aa62d6a0932017e35
shashank_b <bob525test@gmail.com>
2023-05-03 22:32:56 +0530

    Update connector.js

--- CharlieDelta/connector.js
+++ CharlieDelta/connector.js
@@ -1,46 +1,55 @@
 const oracledb = require('oracledb');
+const readline = require('readline');
 
 // Connection details
-
 const dbConfig = {
   user: 'C##BUNNYDRIVE',
   password: 'newpassword',
   connectString: 'localhost'
 };
 
-async function testConnection() {
-  let connection;
+let connection;
 
+async function connect() {
   try {
     // Establish a connection to the database
     connection = await oracledb.getConnection(dbConfig);
 
     console.log('Connected to Oracle Database');
 
-    // Check if the connection is open
-    if (connection && connection.isConnected()) {
+    // Execute a query to check if the connection is open
+    const result = await connection.execute('SELECT 1 FROM DUAL');
+    if (result.rows.length > 0) {
       console.log('Connection is open');
-    } 
-    
-    else {
+    } else {
       console.log('Connection is not open');
     }
+
+    // Start listening for user input
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('Press any key to close the connection...', async () => {
+      rl.close();
+      await closeConnection();
+      console.log('Connection closed');
+    });
   } catch (err) {
     console.error('Error connecting to Oracle Database:', err);
-  } finally {
-    // Release the connection
-    if (connection) {
-      try {
-        await connection.close();
-        console.log('Connection closed');
-      } catch (err) {
-        console.error('Error closing connection:', err);
-      }
-    }
   }
 }
 
+async function closeConnection() {
+  try {
+    // Release the connection
+    if (connection) {
+      await connection.close();
+    }
+  } catch (err) {
+    console.error('Error closing connection:', err);
+  }
+}
 
-
- 
-testConnection()
+connect();
